e79d5b752420ff7c8e77d2c3277ef1743ab8efe9
white-devil-hoster <7425264@gmail.com>
2020-12-25 21:39:30 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// White Devil @ Devilshalani javascript ES6
+gsap.to('.moon', {
+	scrollTrigger: {
+		scrub: true
+	},
+	scale: 2
+})
+
+gsap.to('.sky', {
+	scrollTrigger: {
+		scrub: true
+	},
+	x: - innerWidth / 2
+})
+
+gsap.to('.cloud1', {
+	scrollTrigger: {
+		scrub: true
+	},
+	x: - innerWidth / 3,
+	scale: 1.1
+})
+
+gsap.to('.cloud2', {
+	scrollTrigger: {
+		scrub: true
+	},
+	x: innerWidth / 3,
+	y: innerHeight / 3,
+	scale: 1.1
+})
+
+gsap.to('.santa', {
+	scrollTrigger: {
+		scrub: true
+	},
+	x: innerWidth * 2,
+	y: - innerHeight,
+	scale: 1.3
+})
+
+gsap.to('.tree', {
+	scrollTrigger: {
+		scrub: true
+	},
+	backgroundPosition: innerWidth,
+})
+
+gsap.to('.wish', {
+	scrollTrigger: {
+		scrub: true
+	},
+	y: - 900
+})
+
+window.addEventListener("scroll", () => {
+	let intro = document.querySelector('.intro');
+	intro.style.transform = 'translate(-50%, '+ window.scrollY + 'px)';
+});
